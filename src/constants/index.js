9cc78de716a9fa73bb14633721de0f06cdddc25e
orenmagid <autoimpedicis@gmail.com>
2018-09-10 10:33:45 -0400

    Working on getting web sockets working on Heroku.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
-export const API_ROOT = "http://localhost:3000/api/v1";
-export const API_WS_ROOT = "ws://localhost:3000/cable";
+// export const API_ROOT = "http://localhost:3000/api/v1";
+// export const API_WS_ROOT = "ws://localhost:3000/cable";
 
-// export const API_ROOT = "https://grpball-backend.herokuapp.com/api/v1";
-// export const API_WS_ROOT = "ws://grpball-backend.herokuapp.com/cable";
+export const API_ROOT = "https://grpball-backend.herokuapp.com/api/v1";
+export const API_WS_ROOT = "wss://grpball-backend.herokuapp.com/cable";
 
 export const HEADERS = {
   "Content-Type": "application/json",
@@ -10,8 +10,8 @@ export const HEADERS = {
   Authorization: `Bearer ${localStorage.getItem("token")}`
 };
 
-export const baseUrl = "http://localhost:3000/api/v1";
-// export const baseUrl = "https://grpball-backend.herokuapp.com/api/v1";
+// export const baseUrl = "http://localhost:3000/api/v1";
+export const baseUrl = "https://grpball-backend.herokuapp.com/api/v1";
 
-export const baseUrlForFeed = "https://localhost:300";
-// export const baseUrlForFeed = "https://grpball-backend.herokuapp.com";
+// export const baseUrlForFeed = "https://localhost:300";
+export const baseUrlForFeed = "https://grpball-backend.herokuapp.com";
