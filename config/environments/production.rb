20ce8f6c53d8b6a15d87d0ec03fff2705abee3b1
orenmagid <autoimpedicis@gmail.com>
2018-09-10 10:33:57 -0400

    Websockets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.web_socket_server_url = "wss://grpball-backend.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https:/grpball.herokuapp.com']
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
